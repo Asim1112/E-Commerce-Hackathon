8ccbe8c7db7849d3aad65abcb2c88aff4c5c1c54
Asim Hussain <asimhussain787@gmail.com>
2025-01-19 13:16:33 +0500

    Added PNG favicon and updated layout.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,74 +4,78 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className='flex justify-center'>
-        <div className='w-full max-w-[1440px] h-[auto] bg-black px-4 sm:px-6 md:px-10 lg:px-12'>
+        <div className='w-[1440px] h-[331px] bg-black px-10'>
             
-            <div className='flex flex-col sm:flex-row sm:gap-8 px-2'>
-              {/* Column 1 */}
-              <div className="text-white font-[Helvetica Neue] pt-10">
-                  <ul className="font-medium text-[10px] leading-[32.67px] sm:text-[11px]">
-                      <li><a href="#">FIND A STORE</a></li>
-                      <li><a href="#">BECOME A MEMEBER</a></li>
-                      <li><a href="#">SIGN UP FOR EMAIL</a></li>
-                      <li><a href="#">Send Us Feedback</a></li>
-                      <li><a href="#">STUDENT DISCOUNTS</a></li>
-                  </ul>
-              </div>
+               <div className='flex gap-32 px-2'>
 
-              {/* Column 2 */}
-              <div className="text-white font-[Helvetica Neue] pt-10">
-                  <ul className="text-[11px] leading-[28px] font-medium font-[Helvetica Neue] sm:text-[12px]">
-                      <li><a href="#">GET HELP</a></li>
-                      <li className='text-[#7E7E7E]'><a href="#">Order Status</a></li>
-                      <li className='text-[#7E7E7E]'><a href="#">Delivery</a></li>
-                      <li className='text-[#7E7E7E]'><a href="#">Returns</a></li>
-                      <li className='text-[#7E7E7E]'><a href="#">Payment Options</a></li>
-                      <li className='text-[#7E7E7E]'><a href="#">Contact Us On Nike.com Inquiries</a></li>
-                      <li className='text-[#7E7E7E]'><a href="#">Contact Us On All Other Inquiries</a></li>
-                  </ul>
-              </div>
+<div className="text-white font-[Helvetica Neue] pt-10">
+    <ul className="font-medium text-[10px] leading-[32.67px] font-[Helvetica Neue]">
+        <li><a href="#">FIND A STORE</a></li>
+        <li><a href="#">BECOME A MEMEBER</a></li>
+        <li><a href="#">SIGN UP FOR EMAIL</a></li>
+        <li><a href="#">Send Us Feedback</a></li>
+        <li><a href="#">STUDNET DISCOUNTS</a></li>
+    </ul>
+</div>
 
-              {/* Column 3 */}
-              <div className="text-white font-[Helvetica Neue] pt-10">
-                  <ul className='font-[Helvetica Neue] text-[11px] font-normal leading-[28px] sm:text-[12px]'>
-                      <li><a href="#">ABOUT NIKE</a></li>
-                      <li className='text-[#7E7E7E]'><a href="#">News</a></li>
-                      <li className='text-[#7E7E7E]'><a href="#">Careers</a></li>
-                      <li className='text-[#7E7E7E]'><a href="#">Investors</a></li>
-                      <li className='text-[#7E7E7E]'><a href="#">Sustainability</a></li>
-                  </ul>
-              </div>
-            </div>
+<div className="text-white font-[Helvetica Neue] pt-10">
+    <ul className="text-[11px] leading-[28px] font-medium font-[Helvetica Neue]">
+        <li><a href="#">GET HELP</a></li>
+        <li className='text-[#7E7E7E]'><a href="#">Order Status</a></li>
+        <li className='text-[#7E7E7E]'><a href="#">Delivery</a></li>
+        <li className='text-[#7E7E7E]'><a href="#">Returns</a></li>
+        <li className='text-[#7E7E7E]'><a href="#">Payment Options</a></li>
+        <li className='text-[#7E7E7E]'><a href="#">Contact Us On Nike.com Inquiries</a></li>
+        <li className='text-[#7E7E7E]'><a href="#">Contact Us On All Other Inquiries</a></li>
+    </ul>
+</div>
 
-            {/* Social Media Icons */}
-            <div className="flex justify-center sm:justify-end gap-3 mt-4 sm:mt-0 relative -top-6 sm:-top-48">
-              <a href="#"><Image src="/twitter.svg" alt="twitter" width={30} height={30}/></a>
-              <a href="#"><Image src="/facebook.svg" alt="facebook" width={30} height={30}/></a>
-              <a href="#"><Image src="/youtube.svg" alt="youtube" width={30} height={30}/></a>
-              <a href="#"><Image src="/insta.svg" alt="instagram" width={30} height={30}/></a>
-            </div>
+<div className="text-white font-[Helvetica Neue] pt-10">
+    <ul className='font-[Helvetica Neue] text-[11px] font-normal leading-[28px]'>
+        <li><a href="#">ABOUT NIKE</a></li>
+        <li className='text-[#7E7E7E]'><a href="#">News</a></li>
+        <li className='text-[#7E7E7E]'><a href="#">Careers</a></li>
+        <li className='text-[#7E7E7E]'><a href="#">Investors</a></li>
+        <li className='text-[#7E7E7E]'><a href="#">Sustainability</a></li>
+    </ul>
+</div>
+            
+</div>
 
-            {/* Footer Bottom */}
-            <div className="flex justify-between items-center pt-8 w-full">
-              <div className="flex items-center gap-3">
-                <Image src="pointer.svg" alt="pointer" width={9.38} height={13.12}/>
-                <p className="font-normal text-[12px] text-white leading-[12px] font-[Helvetica Neue]">India</p>
-                <p className="leading-[12px] text-[11px] text-[#7E7E7E] font-normal font-[Helvetica Neue] pl-4">
-                  &copy; 2023 Nike, Inc. All Rights Reserved
-                </p>
-              </div>
 
-              {/* Footer Links */}
-              <ul className="flex gap-6 text-[#7E7E7E] font-medium text-[11px] leading-[12px] font-[Helvetica Neue] items-center sm:text-[12px]">
-                  <li><a href="#">Guides</a></li>
-                  <li><a href="#">Terms of Sale</a></li>
-                  <li><a href="#">Terms of Use</a></li>
-                  <li><a href="#">Nike Privacy Policy</a></li>
-              </ul>
-            </div>
-        </div>
-    </div>            
+<div className="flex justify-end gap-3 relative -top-48">
+  
+  <a href="#"><Image src="/twitter.svg" alt="twitter" width={30} height={30}/></a>
+  <a href="#"><Image src="/facebook.svg" alt="twitter" width={30} height={30}/></a>
+  <a href="#"><Image src="/youtube.svg" alt="twitter" width={30} height={30}/></a>
+  <a href="#"><Image src="/insta.svg" alt="twitter" width={30} height={30}/></a>
+    
+</div>
+
+                        
+  
+
+<div className="flex justify-between items-center pt-8 w-full px-4">
+<div className="flex items-center gap-3">
+
+    <Image src="pointer.svg" alt="pointer" width={9.38} height={13.12}/>
+    <p className="font-normal text-[12px] text-white leading-[12px] font-[Helvetica Neue]">India</p>
+    <p className="leading-[12px] text-[11px] text-[#7E7E7E] font-normal font-[Helvetica Neue] pl-4">
+    &copy; 2023 Nike, Inc. All Rights Reserved</p>
+
+</div>
+
+<ul className="flex gap-6 text-[#7E7E7E] font-medium text-[11px] leading-[12px] font-[Helvetica Neue] items-center">
+    <li><a href="#">Guides</a></li>
+    <li><a href="#">Terms of Sale</a></li>
+    <li><a href="#">Terms of Use</a></li>
+    <li><a href="#">Nike Privacy Policy</a></li>
+</ul>
+</div>
+</div>            
+</div>  
+    
   )
 }
 
-export default Footer;
+export default Footer
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,111 +2,128 @@ import React from 'react'
 import Image from "next/image";
 import Link from "next/link";
 
+
 const Header = () => {
   return (
-    <div className="flex justify-center">
-      <div className="w-full max-w-[1440px]">
-
-        {/* Top Bar */}
-        <div className="h-[36px] bg-[rgba(245,245,245,1)] content-center px-4 sm:px-6 md:px-12">
-          <div className="flex justify-between items-center">
-            <div>
-              <Image 
-                src="/logo-icon.svg"
-                alt="logo design"
-                width={24}
-                height={24}
-              />
-            </div>
-
-            <div>
-              <ul className="flex gap-4 font-[Helvetica Neue] font-medium text-[10px] sm:text-[11px]">
-                <li><a href="#" className="text-black hover:text-blue-500 transition-colors duration-300">Find a Store</a></li>
-                <li>|</li>
-                <li><Link href="/Help" className="text-black hover:text-blue-500 transition-colors duration-300">Help</Link></li>
-                <li>|</li>
-                <li><Link href="/JoinUs" className="text-black hover:text-blue-500 transition-colors duration-300">Join Us</Link></li>
-                <li>|</li>
-                <li><Link href="/Login" className="text-black hover:text-blue-500 transition-colors duration-300">Sign In</Link></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        {/* Main Navigation Bar */}
-        <div className="flex justify-between px-4 sm:px-6 md:px-10 w-full h-[60px] top-[36px] items-center">
-          <div>
-            <Link href="/">
-              <Image 
-                src="/nike-logo.svg"
-                alt="logo icon"
-                width={78.47}
-                height={78.47}
-              />
-            </Link>
-          </div>
-
-          {/* Navbar Links */}
-          <div className="font-Helvetica Neue text-[13px] sm:text-[14px] md:text-[15px] font-medium pl-4 sm:pl-10 md:pl-32">
-            <ul className="flex gap-4 sm:gap-6 md:gap-8">
-              <li><Link href="/NewFeatured" className="hover:text-blue-500 transition-colors duration-300">New & Featured</Link></li>
-              <li><Link href="/Men" className="hover:text-blue-500 transition-colors duration-300">Men</Link></li>
-              <li><Link href="/Women" className="hover:text-blue-500 transition-colors duration-300">Women</Link></li>
-              <li><a href="#" className="hover:text-blue-500 transition-colors duration-300">Kids</a></li>
-              <li><a href="#" className="hover:text-blue-500 transition-colors duration-300">Sale</a></li>
-              <li><a href="#" className="hover:text-blue-500 transition-colors duration-300">SNKRS</a></li>
-            </ul>
-          </div>
-
-          {/* Search Bar and Icons */}
-          <div className="flex items-center gap-4 sm:gap-6 md:gap-8">
-
-            {/* Search Bar */}
-            <div className="relative w-[150px] sm:w-[180px] md:w-[220px]">
-              <input
-                type="text"
-                placeholder="Search"
-                className="w-full h-[40px] rounded-[100px] bg-gray-100 pl-12 text-sm sm:text-base"
-              />
-              <Image 
-                src="/search-mirror-icon.svg"
-                alt="search bar icon"
-                width={16}
-                height={16}
-                className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5"
-              />
-            </div>
-
-            {/* Heart and Cart Icons */}
-            <div className="flex gap-6 items-center">
-              {/* Heart Icon */}
-              <Link href="#">
-                <Image 
-                  src="/icon-heart.svg"
-                  alt="heart icon"
-                  width={19.5}
-                  height={16.76}
-                  className="w-[24px] h-[20px] sm:w-[30px] sm:h-[22px]"
-                />
-              </Link>
-
-              {/* Cart Icon */}
-              <Link href="/Cart">
-                <Image 
-                  src="/icon-box.svg"
-                  alt="cart icon"
-                  width={16.5}
-                  height={16.5}
-                  className="w-[30px] h-[30px] sm:w-[35px] sm:h-[35px]"
-                />
-              </Link>
-            </div>
-
-          </div>
-        </div>
-
-      </div>
+    
+<div className="flex justify-center">
+<div className="w-[1440px]">
+  <div className="h-[36px] bg-[rgba(245,245,245,1)] content-center px-12">
+      <div className="flex justify-between">
+      <div>
+                 
+    <Image 
+    src="/logo-icon.svg"
+    alt="logo design"
+    width={24}
+    height={24}
+                    
+    />
     </div>
+
+<div>
+  <ul className="flex gap-4 font-[Helvetica Neue] font-medium text-[11px]">
+    
+    <li><a href="#" className="text-black hover:text-blue-500 transition-colors duration-300">Find a Store</a></li>
+    <li>|</li>
+    <li><Link href="/Help" className="text-black hover:text-blue-500 transition-colors duration-300">Help</Link></li>
+    <li>|</li>
+    <li><Link href="/JoinUs" className="text-black hover:text-blue-500 transition-colors duration-300">Join Us</Link></li>
+    <li>|</li>
+    <li><Link href="/Login" className="text-black hover:text-blue-500 transition-colors duration-300">Sign In</Link></li>
+              
+  </ul>
+</div>
+  
+</div>
+</div>
+
+
+
+
+<div className="flex justify-between px-10 w-[1440px] h-[60px] top-[36px] items-center">
+      
+<div>
+    <Link href="/">
+    <Image 
+    src="/nike-logo.svg"
+    alt="logo icon"
+    width={78.47}
+    height={78.47}/>
+    </Link>
+</div>
+
+
+
+{/* NAVBAR LINKS AND SEARCHBAR */}
+  
+<div className="font-Helvetica Neue text-[15px] font-medium pl-32">
+  
+  <ul className="flex gap-6">
+    <li><Link href="/NewFeatured" className="hover:text-blue-500 transition-colors duration-300">New & Featured</Link></li>
+    <li><Link href="/Men" className="hover:text-blue-500 transition-colors duration-300">Men</Link></li>
+    <li><Link href="/Women" className="hover:text-blue-500 transition-colors duration-300">Women</Link></li>
+    <li><a href="#" className="hover:text-blue-500 transition-colors duration-300">Kids</a></li>
+    <li><a href="#" className="hover:text-blue-500 transition-colors duration-300">Sale</a></li>
+    <li><a href="#" className="hover:text-blue-500 transition-colors duration-300">SNKRS</a></li>
+  </ul>
+  
+  </div>
+                  
+  
+  <div className="flex items-center gap-8">
+  
+  <div className="relative">
+      <input
+      type="text"
+      placeholder="Search" 
+      className="w-[180px] h-[40px] rounded-[100px] bg-gray-100 pl-12"
+        />
+    
+    <Image 
+    src="/search-mirror-icon.svg"
+    alt="search bar icon"
+    width={5}
+    height={5}
+    className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5"
+    />
+  </div>
+
+    
+    
+    
+  {/* Heart and box logo */}
+  
+  
+  <div className="flex justify-between items-center w-[84px] h-[50px]">
+    <Link href="#">
+    <Image 
+    src="/icon-heart.svg"
+    alt="heart icon"
+    width={19.5}
+    height={16.76}
+    className="w-[30.5px] h-[22.76px]"
+    />
+    </Link>
+    
+
+  <Link href="/Cart">
+  <Image 
+    src="/icon-box.svg"
+    alt="heart box"
+    width={16.5}
+    height={16.5}
+    className="w-[35.5px] h-[35.5px]"
+    />
+  </Link>
+  
+  </div>
+  </div>
+
+</div>
+
+</div>
+</div>
   )
 }
 
